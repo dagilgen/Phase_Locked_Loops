54f25bee678890ac2b89f5a733860fb24c5a84dd
TheFabiator <martif@student.ethz.ch>
2014-03-01 17:06:14 +0100

    PLL implementation extended to harmonic signals. Module utilities.py added to construct block diagonal matrices that are required to simulatea superposition of several sinusoids with different frequency.

--- Phase_Locked_Loop/main.py
+++ Phase_Locked_Loop/main.py
@@ -6,66 +6,127 @@ Created on 26.02.2014
 import numpy as np
 import pylab as py
 import message as msg
-#import utilities as util
+import utilities as util
 from matplotlib import rc
 
 
 def main():
     # Parameters
-    nOfSteps = 1000
-    steps = np.arange(nOfSteps)
-    phi = 90
+    nOfSteps = 288
     omega = 2*np.pi/144
-    amplitude = 5
+    phi = [0,np.pi/4,np.pi/2,np.pi]
+    harmonicFrequencies = [1,2,3]
+    amplitudes = [5,2,3]
     variance = 2
     gamma = 1
     
-    A = np.array([[np.cos(omega),np.sin(omega)],[-np.sin(omega),np.cos(omega)]])
-    C = amplitude*np.array([[1,0]])
+    
+    # Generation of the state space model matrices
+    nOfFrequencies = len(harmonicFrequencies)
+    matrixList = []
+    C = np.zeros((1,2*nOfFrequencies))
+
+    for i in range(0,nOfFrequencies):
+        f = harmonicFrequencies[i]
+        rotationalMatrix = np.array([[np.cos(f*omega),np.sin(f*omega)],[-np.sin(f*omega),np.cos(f*omega)]])
+        matrixList.append(rotationalMatrix)
+        C[0,2*i] = amplitudes[i]
+    A = util.blockDiag(matrixList)
+    
     
     # Computation of the inverse of A (Remains fixed)
-    I = np.identity(A.shape[1])
+    I = np.identity(A.shape[0])
     A_inv = np.linalg.solve(A, I)
     
+    
     # Initial values
-    x_k = np.array([[np.cos(phi)],[np.sin(phi)]])
-    W_x = np.identity(A.shape[0])
-    Wm_x = np.array([[1],[0]])
+    x_k = np.zeros((2*nOfFrequencies,1))
+    for i in range(0,nOfFrequencies):
+        f = harmonicFrequencies[i]
+        x_k[2*i,0] = np.cos(phi[i])
+        x_k[2*i+1,0] = np.sin(phi[i])
+        
+    W_x = I
+    Wm_x = np.transpose(np.tile(np.array([[1,0]]),nOfFrequencies))
     y = np.zeros(nOfSteps)
     y_tilde = np.zeros(nOfSteps)
-    phase = np.zeros(nOfSteps)
+    phase = np.zeros((nOfSteps,nOfFrequencies))
+    
     
     # Solving the PLL problem iteratively via factor graphs
+    steps = np.arange(1,nOfSteps+1)
     for step in steps:
         x_k = np.dot(A, x_k)
         y_k = np.dot(C, x_k)
-        y[step] = y_k
+        y[step-1] = y_k
         z_k = variance*np.random.rand()
         y_tildek = y_k+z_k
-        y_tilde[step] = y_tildek
+        y_tilde[step-1] = y_tildek
+        
         
         # Apply the message passing algorithm
         [W_x, Wm_x] = msg.directForwardMessagePassing(A_inv, C, variance, y_tildek, W_x, Wm_x)
         mean_k = np.linalg.solve(W_x, Wm_x)
-        alpha = 1/(np.sqrt(mean_k[0]**2+mean_k[1]**2))  # Eliminate rounding errors
-        phase_k = np.arccos(alpha*mean_k[0])
-        phase[step] = phase_k
+        
+        # Compute phases of harmonic sinusoids
+        for i in range(0,nOfFrequencies):
+            alpha = 1/(np.sqrt(mean_k[2*i]**2+mean_k[2*i+1]**2))  # Scale mean value
+            phase_k = np.arccos(alpha*mean_k[2*i])
+            phase[step-1,i] = phase_k
         
         # Incorporate forgetting factor
         W_x = W_x/gamma
         Wm_x = Wm_x/gamma
         
-    # Plot graphs
-    py.subplot(4,1,1)
+    estimatedHarmonicSig = np.zeros(nOfSteps)
+    for i in range(0,nOfFrequencies):
+        estimatedHarmonicSig += amplitudes[i]*np.cos(phase[:,i])
+    
+    
+    # Enable Latex functionality and fonts
+    rc('text',usetex=True)
+    rc('font',**{'family':'serif','serif':['Computer Modern']})
+    
+    
+    # Plot estimation results
+    py.figure(1)
+    
+    py.subplot(2,2,1)
     py.plot(steps,y)
-    py.subplot(4,1,2)
+    py.title('Reference input signal')
+    py.xlabel('Timestep $k$')
+    py.ylabel('Amplitude')
+    
+    py.subplot(2,2,2)
     py.plot(steps,y_tilde)
-    py.subplot(4,1,3)
-    py.plot(steps,phase,'x')
-    py.subplot(4,1,4)
-    py.plot(steps,amplitude*np.cos(phase))
+    py.title('Noisy signal with $\sigma^2$ = ' + str(variance))
+    py.xlabel('Timestep $k$')
+    py.ylabel('Amplitude')
+    
+    py.subplot(2,2,3)
+    py.plot(steps,phase[:,1],'x')
+    py.title('Estimated phase')
+    py.xlabel('Timestep $k$')
+    py.ylabel('Amplitude')
+    
+    py.subplot(2,2,4)
+    py.plot(steps,estimatedHarmonicSig)
+    py.title('Estimated signal')
+    py.xlabel('Timestep $k$')
+    py.ylabel('Amplitude')
+    
+    
+    # Plot the harmonics and the resultiing signal
+    py.figure(2)
+    for i in range(0,nOfFrequencies):
+        harmonic = amplitudes[i]*np.cos(steps*harmonicFrequencies[i]*omega+phi[i])
+        py.plot(steps,harmonic,color=util.colors(i),linewidth = 1.5)
+    
+    py.plot(steps,y,color = 'k',linewidth = 2.5)
+    py.title('Reference input signals and corresponding harmonics')
+    py.xlabel('Timestep $k$')
+    py.ylabel('Amplitude')
     py.show()
-        
         
 if __name__ == '__main__':
     
---
+++ Phase_Locked_Loop/utilities.py
@@ -0,0 +1,90 @@
+'''
+Created on 28.02.2014
+
+@author: Fabio Marti
+'''
+
+import numpy as np
+
+
+class Error(Exception):
+    pass
+
+
+def colors(colorIndex):
+    """
+    Returns the respective RGB color for a certain input number.
+    
+    Inputs:
+    
+    colorIndex : int
+        Integer indexing the desired color.
+        
+        
+    Outputs:
+    
+    color : list
+        The selected RGB color.
+    """
+    
+    if colorIndex == 0:             # red
+        color = [1,0.2,0.2]
+    elif colorIndex == 1:           # orange
+        color = [1,0.8,0]
+    elif colorIndex == 2:           # brown
+        color = [0.4,0.4,0]
+    elif colorIndex == 3:           # violet
+        color = [0.2,0.1,0.4]
+    elif colorIndex == 4:           # blue
+        color = [0.2,0.2,1]
+    elif colorIndex == 5:           # light blue
+        color = [0.6,0.6,1]
+    elif colorIndex == 6:           # light green
+        color = [0.3,1,0.3]
+    elif colorIndex == 7:           # green
+        color = [0,0.7,0]
+    elif colorIndex == 8:           # dark green
+        color = [0,0.4,0]
+    elif colorIndex == 9:           # pink
+        color = [1,0,1]
+    else:                           # grey
+        color = [0.6,0.6,0.6]
+    
+    
+    return color
+
+
+def blockDiag(matrixList):
+    """
+    Takes square matrices as input and constructs a block diagonal
+    matrix in order of their appearance as input arguments.
+    
+    Inputs:
+    
+    matrixList : list
+        A list of arrays serving as diagonal matrices of the
+        resulting block diagonal matrix.
+        
+        
+    Outputs:
+    
+    blockMatrix : array_like
+        Resulting block diagonal matrix.
+    """
+    
+    # Check if all input matrices are square matrices
+    dimension = 0
+    for block in matrixList:
+        if block.shape[0] != block.shape[1]:
+            raise Error("Non-square input matrix.")
+        dimension += block.shape[0]
+    
+    # Construct diagonal block matrix
+    index = 0
+    blockMatrix = np.zeros((dimension, dimension))
+    for block in matrixList:
+        matSize = block.shape[0]
+        blockMatrix[index:index+matSize,index:index+matSize] = block
+        index += matSize
+        
+    return blockMatrix
